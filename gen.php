560a453e2e5bae1ee3adc029c8dddce2ebe672cf
bossbaby <3064273390@qq.com>
2022-03-28 17:42:18 +0800

    CAPTCHA Generator

---
+++ gen.php
@@ -0,0 +1,20 @@
+<?php
+include "vendor/autoload.php";
+use Gregwar\Captcha\CaptchaBuilder;
+
+function create_uuid($prefix="") {
+    $chars = md5(uniqid(mt_rand(), true));
+    $uuid = substr ( $chars, 0, 8 ) . '-'
+        . substr ( $chars, 8, 4 ) . '-'
+        . substr ( $chars, 12, 4 ) . '-'
+        . substr ( $chars, 16, 4 ) . '-'
+        . substr ( $chars, 20, 12 );
+    return $prefix.$uuid ;
+}
+
+for ($i = 1; $i <= 10; ++$i) {
+    $builder = new CaptchaBuilder;
+    $dir = 'data1/';
+    $builder->build(90, 35);
+    $builder->save($dir . $builder->getPhrase() . '_' . create_uuid() . '.png');
+}
