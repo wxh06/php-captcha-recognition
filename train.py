51341ffc6453ba550e6e98c909296b9d4fed4373
汪心禾 <wangxinhe06@gmail.com>
2023-02-09 18:18:52 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,9 +1,3 @@
-"""
-captcha-tensorflow
-Copyright (c) 2017 Jackon Yang
-https://github.com/JackonYang/captcha-tensorflow/blob/master/captcha-solver-tf2-4digits-AlexNet-98.8.ipynb
-"""
-
 from datetime import datetime
 from os import listdir, path
 
@@ -18,40 +12,33 @@ LOG_DIR = "logs/fit/" + datetime.now().strftime("%Y%m%d-%H%M%S")
 H, W, C = 35, 90, 3  # height, width, 3 (RGB channels)
 N_LABELS = 128
 D = 4  # num_of_chars_per_image
-EPOCHS = 4
+EPOCHS = 16
 
 
 # create a pandas data frame of images and labels
 files = listdir(DATA_DIR)
 
-
-p = np.random.permutation(len(files))
-train_up_to = int(len(files) * 0.9375)
-train_idx = p[:train_up_to]
-test_idx = p[train_up_to:]
-
-# split train_idx further into training and validation set
-train_up_to = int(train_up_to * 0.9375)
-train_idx, valid_idx = train_idx[:train_up_to], train_idx[train_up_to:]
-
-print(
-    "train count: %s, valid count: %s, test count: %s"
-    % (len(train_idx), len(valid_idx), len(test_idx))
-)
+np.random.shuffle(files)
+split = int(len(files) / 32)
+files_train = files[: -split * 2]
+files_validation = files[-split * 2 : -split]
+files_test = files[-split:]
 
 
-def get_data_generator(files, indices, repeat=1):
-    for _ in range(repeat):
-        for i in indices:
-            df = pd.read_pickle(path.join(DATA_DIR, files[i]))
+def get_data_generator(files, repeat=False):
+    while True:
+        for file in files:
+            df = pd.read_pickle(path.join(DATA_DIR, file))
             images = np.array([a for a in df["image"]]) / 255.0
             labels = np.array(
                 [
-                    [np.array(to_categorical(ord(i), N_LABELS)) for i in label.lower()]
+                    [np.array(to_categorical(ord(i), N_LABELS)) for i in label]
                     for label in df["phrase"]
                 ]
             )
             yield images, labels
+        if not repeat:
+            return
 
 
 input_layer = tf.keras.Input(shape=(H, W, C))
@@ -70,27 +57,37 @@ x = layers.Reshape((D, N_LABELS))(x)
 
 model = models.Model(inputs=input_layer, outputs=x)
 
-model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
+model.compile(
+    optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"]
+)
 model.summary()
 
 
+class EpochSaver(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs):
+        self.model.save("saved_model/model_{}".format(epoch))
+
+
 history = model.fit(
-    get_data_generator(files, train_idx, EPOCHS),
-    steps_per_epoch=len(train_idx),
+    get_data_generator(files_train, True),
+    steps_per_epoch=len(files_train),
     epochs=EPOCHS,
-    validation_data=get_data_generator(files, valid_idx, EPOCHS),
-    validation_steps=len(valid_idx),
-    callbacks=[tf.keras.callbacks.TensorBoard(log_dir=LOG_DIR, histogram_freq=1)],
+    validation_data=get_data_generator(files_validation, True),
+    validation_steps=len(files_validation),
+    callbacks=[
+        tf.keras.callbacks.TensorBoard(log_dir=LOG_DIR, histogram_freq=1),
+        EpochSaver(),
+    ],
 )
 
-
 # evaluate loss and accuracy in test dataset
-test_gen = get_data_generator(files, test_idx)
 print(
     dict(
         zip(
             model.metrics_names,
-            model.evaluate(test_gen, steps=len(test_idx)),
+            model.evaluate(
+                get_data_generator(files_test), steps=len(files_test)
+            ),
         )
     )
 )
